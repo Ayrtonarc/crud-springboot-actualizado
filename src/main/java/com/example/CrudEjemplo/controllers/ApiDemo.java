214908ea4f47d1c73a86c059345af2fb455859be
Ayrton Santos <ayrton.arc@gmail.com>
2023-01-11 22:10:04 -0800

    Get all users & get user ByID

--- src/main/java/com/example/CrudEjemplo/controllers/ApiDemo.java
+++ src/main/java/com/example/CrudEjemplo/controllers/ApiDemo.java
@@ -1,15 +1,36 @@
 package com.example.CrudEjemplo.controllers;
 
+import com.example.CrudEjemplo.model.Usuario;
+import com.example.CrudEjemplo.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api")
 public class ApiDemo {
 
+    @Autowired
+    UsuarioService usuarioService; //inyectar el servicio
     @GetMapping("/saludar")
     public String saludar(){
         return "Hola m desde ApiDemo";
     }
+
+    //api
+    @GetMapping("/all")
+    public ArrayList<Usuario> getAllUser(){
+        return usuarioService.getAllUser();
+    }
+
+    @GetMapping("/find/{id}")
+    public Optional<Usuario> getUserById(@PathVariable("id")long id){
+        return usuarioService.getUserById(id);
+    }
+
 }
