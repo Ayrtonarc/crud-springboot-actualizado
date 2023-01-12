fa3273220efc5efb2217ed1a82371f972941e87e
Ayrton Santos <ayrton.arc@gmail.com>
2023-01-11 20:37:31 -0800

    se crea una ruta para test wab
    servidor funcionando

--- src/main/java/com/example/CrudEjemplo/controllers/ApiDemo.java
+++ src/main/java/com/example/CrudEjemplo/controllers/ApiDemo.java
@@ -1,4 +1,15 @@
 package com.example.CrudEjemplo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
 public class ApiDemo {
+
+    @GetMapping("/saludar")
+    public String saludar(){
+        return "Hola m desde ApiDemo";
+    }
 }
