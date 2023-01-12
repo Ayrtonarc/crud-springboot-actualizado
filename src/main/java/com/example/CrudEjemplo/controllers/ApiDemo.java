c06f8580b72fd8be3a4282cdc02e3f254e6f417d
Ayrton Santos <ayrton.arc@gmail.com>
2023-01-11 22:20:13 -0800

    guardar editar usuario

--- src/main/java/com/example/CrudEjemplo/controllers/ApiDemo.java
+++ src/main/java/com/example/CrudEjemplo/controllers/ApiDemo.java
@@ -3,10 +3,7 @@ package com.example.CrudEjemplo.controllers;
 import com.example.CrudEjemplo.model.Usuario;
 import com.example.CrudEjemplo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -31,6 +28,10 @@ public class ApiDemo {
     @GetMapping("/find/{id}")
     public Optional<Usuario> getUserById(@PathVariable("id")long id){
         return usuarioService.getUserById(id);
+    }
+    @PostMapping("/save")
+    public Usuario saveUser(@RequestBody Usuario u){
+        return usuarioService.saveUser(u);
     }
 
 }
